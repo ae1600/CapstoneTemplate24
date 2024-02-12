4094b23a3ffd129e4ce37b9b98a2aa31ae5d270d
ae1600 <xdatrallis@gmail.com>
2024-02-12 11:24:03 -0800

    updated setup

--- app/utils/secrets copy.py
+++ app/utils/secrets copy.py
@@ -2,8 +2,8 @@ import os
 
 def getSecrets():
     secrets = {
-        'MONGO_HOST':"",
-        'MONGO_DB_NAME':"",
+        'MONGO_HOST':"mongodb+srv://ae742750:Alex2006@alex.1klxjm2.mongodb.net/Alex?retryWrites=true&w=majority",
+        'MONGO_DB_NAME':"Alex",
         'GOOGLE_CLIENT_ID': "",
         'GOOGLE_CLIENT_SECRET':"",
         'GOOGLE_DISCOVERY_URL':"https://accounts.google.com/.well-known/openid-configuration",
